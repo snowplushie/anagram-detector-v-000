f717ebfa49230e86ac665bc582e2fc0259583fc5
Paige Wiener <snowplushie@yahoo.com>
2019-03-13 06:39:22 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,14 @@
 # Your code goes here!
+class Anagram
+  attr_accessor :words
+  
+  def initialize(words)
+    @words = words
+  end
+  
+  def match(words)
+    words.select do |words|
+      words.split("").sort == @words.split("").sort
+    end
+  end
+end
